bb427970f71e77220414f2617357da72b857075f
benhdev <benpaulhirst@gmail.com>
2017-07-11 19:08:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/plain");
   
-	res.end(pageInfo); 
+	res.end(emailInfo + "\n \n" + pageInfo); 
   
 });
 
