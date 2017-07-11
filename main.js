33ac894bde268b15240e786097a124dc8343774c
benhdev <benpaulhirst@gmail.com>
2017-07-11 19:01:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var pageInfo = false;
+var emailInfo = false;
+var prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['email'], function (err, result) {
+	emailInfo = result.email;
+});
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const req = http.request("http://teamupgrade.co.uk/", (res) => {  
+	res.on('data', function(data) {
+		pageInfo = data;
+	});
+});
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+  
+	res.end(pageInfo); 
+  
+});
+
+req.end()
+
+server.listen(port, hostname, () => {
+//	console.log(`Server running at http://${hostname}:${port}/`);
+});
