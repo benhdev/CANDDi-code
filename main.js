caf3d411a5c65793b32ac9973d062ca036c90533
benhdev <benpaulhirst@gmail.com>
2017-07-12 13:54:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
 var pageInfo = "";
 var emailInfo = "";
-var prompt = require('prompt');
+var prompt = require("prompt");
+var pattern = "[A-z0-9].+[@][A-z0-9]+[.][A-z0-9]+";
+var regx = new RegExp(pattern, "g");
+
+// Implementation of the web crawler
+var crawler = require("crawler");
+var cr = new crawler({
+	maxConnections : 10,
+	
+	callback : function(error, res, done) {
+		if(error) {
+			console.log(error);
+		} else {
+			var $ = res.$;
+			var result;
+			var resCount = 0;
+			while(result = regx.exec($("html").text())) {
+				var match = result[0];
+				console.log(match);
+			}
+		}
+		done();
+	}
+});
 
 prompt.start();
 
-prompt.get(['email'], function (err, result) {
+prompt.get(["email"], function (err, result) {
 	emailInfo = result.email;
 });
 
-const http = require('http');
+const http = require("http");
+const https = require("https");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
-
-const req = http.request("http://teamupgrade.co.uk/", (res) => {  
-	res.on('data', function(data) {
-		pageInfo += data;
-	});
-});
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/plain");
-  
-	res.end(emailInfo + "\n \n" + pageInfo); 
-  
+	
+	if(emailInfo != "") {
+		var emailDomain = emailInfo.substring(emailInfo.search("@") + 1);
+		
+		var checked = [];
+		cr.queue("https://www." + emailDomain);
+		
+		const req = https.request("https://www." + emailDomain, (res) => {  
+			res.on("data", function(data) {
+				pageInfo += data;
+			});
+		});
+		
+		req.end();
+	}
+	res.end(emailInfo + "\n \n" + pageInfo);
 });
 
-req.end()
+
 
 server.listen(port, hostname, () => {
 //	console.log(`Server running at http://${hostname}:${port}/`);
